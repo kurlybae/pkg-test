2effd97a9ad6e9c833531a22482fd1845404a1a6
배창원 <changwon.bae@kurlycorp.com>
2023-05-12 11:18:36 +0900

    tttt

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,5 +3,5 @@ import {subtract, sum} from "../services/calculator";
 describe('Calculator Test', () => {
     it('sum', () => expect(sum(1, 1)).toEqual(2));
     it('subtract', () => expect(subtract(3, 1)).toEqual(2));
-    // it('subtract', () => expect(subtract(2, 1)).toEqual(2));
+    // it('subtract', () => expect(subtract(2, 1)).toEqual(2));// d
 });
