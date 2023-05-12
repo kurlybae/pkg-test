2eecc35d4cb6a113cfff78bd5b29607f26721b1e
배창원 <changwon.bae@kurlycorp.com>
2023-05-12 11:12:28 +0900

    dd

--- src/services/calculator.ts
+++ src/services/calculator.ts
@@ -1,3 +1,7 @@
-export function sum(a: number, b: number) {
+export function sum(a: number, b: number):number {
     return a + b
+}
+
+export function subtract(a: number, b: number):number {
+    return a - b
 }
